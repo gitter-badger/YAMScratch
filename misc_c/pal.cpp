59a3ef5ed02676b88d2f1bdb04621f0121733afe
YetAnotherMinion <yetanotherminion@gmail.com>
2014-10-23 11:07:08 -0400

    serial communication

--- misc_c/pal.cpp
+++ misc_c/pal.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		char pal[181];
+		char pal[255];
 		char rev[255];
 		char *qur = argv[1]; //pointer to the string pointer
 		char *foo = argv[1];
@@ -20,8 +20,7 @@ int main(int argc, char *argv[])
 		//bounds checking of input string
 		unsigned int size_input = 1; // use to track overflows of int
 		//we remeber to subtract off 1 later in the size comparison
-		while(*qur++ && size_input){printf("%x\n\r",(char *) qur);size_input++;} //figure out how big the string is
-		printf("size of input: %u\n\r",size_input);
+		while(*qur++ && size_input){size_input++;} //figure out how big the string is
 		if (--size_input < sizeof(pal))
 			{
 			//strcpy
@@ -37,11 +36,59 @@ int main(int argc, char *argv[])
 			while(*++foo){*--right = *foo;}
 			//end reverse strcpy
 			printf("right is : %s\n\r",right);
+			//--------------------------------------------------------------//
+			int max_count = 0;
+			int max_index_start = 0;
+			int max_index_end = 0;
+			int i,j;
+			
+			for(i=0; i<size_input; i++)
+			{
+				char *right_ptr = right;
+				char *left_ptr = pal;
+				int temp_count = 0;
+				int temp_index_start = 0;
+				int temp_index_end = 0;
+
+				for(j = i; j<size_input; j++)
+				{
+					printf("i: %u, j: %u,\n",i,j);
+					printf("leftchar = %c , rightchar = %c\n",*(left_ptr+j), *right_ptr);
+
+					printf("     The largest palindrome is size %u\n",max_count);
+					printf("     The starting index is %x\n", max_index_start);
+					printf("     The end index is :    %x\n\n", max_index_end);
+
+					if (left_ptr[j] == *right_ptr++)
+					{
+						temp_index_start = temp_index_start ? temp_index_start : j;
+						
+						temp_count++;
+						printf("__________same, temp count = %u, max_count = %u\n",temp_count,max_count);
+
+						max_index_end = (max_count < temp_count) ? j : max_index_end;
+						
+						max_count = (max_count < temp_count) ? temp_count : max_count ;
+						printf("The largest palindrome is size %u\n",max_count);
+						printf("The end index is %x\n", max_index_end);
+					}
+					else
+					{
+
+						 //reset the count
+						temp_count = 0; //zero the temp count
+					}
+				}
+			}
+			printf("The largest palindrome is size %u\n",max_count);
+			printf("The starting index is %x\n", max_index_start);
+			printf("The end index is :    %x\n", max_index_end);
 		}
 		else
 		{
 			std::cout << "too big input" << std::endl;
 		}
+
 	}
 	return 0;
 }
