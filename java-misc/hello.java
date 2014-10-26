da3a0037a72086c52511c51b7e453636bc316bee
YetAnotherMinion <yetanotherminion@gmail.com>
2014-10-26 19:33:51 -0400

    more code

--- java-misc/hello.java
+++ java-misc/hello.java
@@ -1,6 +1,16 @@
 class HelloWorld {
 	public static void main(String[] argv){
 		System.out.println("HelloWorld"); 
+		try{
+			System.out.println(HelloWorld.foo(4));
+		}
+		catch(IndexOutOfBoundsException e){
+			System.err.println(e);
+		}
 		// this is a comment
+	}
+	private static int foo(int  bar){
+		int[] f = new int[2];
+		return f[bar];
 	}
 }
