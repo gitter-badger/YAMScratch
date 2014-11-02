1a653b868bce65325febe7d7a8b396da03d7ecb8
YetAnotherMinion <yetanotherminion@gmail.com>
2014-11-01 22:45:50 -0400

    enums

---
+++ java-misc/fizzbuzz.java
@@ -0,0 +1,43 @@
+
+
+class FizzBuzz
+{
+	public static final String batman = "batman";
+	public boolean my_flag;
+	public static void main (String[] argv)
+	{
+		for( int i = 0; i <100; i++)
+		{
+
+		}
+		FizzBuzz some_state = new FizzBuzz(State.WORRIED);
+		some_state.PrintTheState();
+
+	}
+
+	State zoop;
+	public FizzBuzz(State bar)
+	{
+		this.zoop = bar;
+	}
+	private void PrintTheState()
+	{
+		switch (zoop)
+		{
+			case HAPPY:
+				System.out.println("State is HAPPY");
+				break;
+			case SAD:
+				System.out.println("State is SAD");
+			case WORRIED:
+				System.out.println("State is WORRIED");
+			case SEVEN:
+				System.out.println("State is SEVEN");
+		}
+		System.out.println((zoop == State.WORRIED) ? "true": "false");
+	} 
+
+	public enum State {
+		HAPPY, SAD, WORRIED, SEVEN
+	}
+}
