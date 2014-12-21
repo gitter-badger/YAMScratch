78fb06d738eb3a3b8da2f8cdeb329a5b7d1b7695
YetAnotherMinion <yetanotherminion@gmail.com>
2014-12-20 21:54:40 -0500

    better table creation

--- motivator/extension_loader_hack.py
+++ motivator/extension_loader_hack.py
@@ -11,43 +11,39 @@ if len(sys.argv) == 3:
 	database_name = sys.argv[2]
 	package_name = sys.argv[1]
 
-	def validate_database_name(db_name):
-		valid = {"yes":True,"y":True,"": True,
-				 "no":False, "n":False}
-		if not ".sqlite" in db_name:
-			#test for valid sqlite database name
-			print "ASSUMING extension {}.sqlite ".format(db_name)
-			db_name += ".sqlite"
-		if not os.path.exists(db_name):
-			while True:
-				#run until we get a valid response
-				resp = raw_input("Database does not exist. Do you want to create one? [Y/n]").lower()
-				if resp in valid:
-					if not valid[resp]:
-						print "not valid"
-						continue
-					else:
-						print "Creating... {}".format(db_name)
-						return db_name
-		else:
-			return db_name
-
-	database_name = validate_database_name(database_name)
+	valid = {"yes":True,"y":True,"": True,
+			 "no":False, "n":False}
+	if not ".sqlite" in database_name:
+		#test for valid sqlite database name
+		print "ASSUMING extension {}.sqlite ".format(database_name)
+		database_name += ".sqlite"
+	if not os.path.exists(database_name):
+		while True:
+			#run until we get a valid response
+			resp = raw_input("Database \"{}\" does not exist. Do you want to create it? [Y/n]".format(database_name)).lower()
+			if resp in valid:
+				if not valid[resp]:
+					#we did not want to create database
+					print "Exiting..."
+					database_name = None
+					break
+				else:
+					print "Creating... {}".format(database_name)
+					break
 	if not database_name:
-		pass
-
-
-
-			
-
-
+		exit()
 	conn = sqlite3.connect(database_name)
 	cur = conn.cursor()
-	cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='extensions'")
+	#parameterise the table name
+	exten_tbname = "extensions"
+	#check for presence of table
+	cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='{}'".format(exten_tbname))
 	empty_test = cur.fetchone()
 	if empty_test is None:
-		cur.execute('CREATE TABLE extensions (language text,extension text)')
+		print "Creating TABLE \"{}\"".format(exten_tbname)
+		conn.execute('CREATE TABLE %s (language text,extension text)' % exten_tbname)
 	#now that the table exists
+
 
 	while True:
 		a = raw_input(">>")
