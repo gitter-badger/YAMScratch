d926a7dcf71d4a2b5cb3a4f4ce4e3c213ce44475
YetAnotherMinion <yetanotherminion@gmail.com>
2014-12-23 15:22:58 -0500

    rough centering algorithm

--- python-misc/shape_drawer.py
+++ python-misc/shape_drawer.py
@@ -24,38 +24,71 @@ def draw_line(x0,y0,x1,y1):
 	print_board(board)
 
 def draw_circle(radius,x0,y0):
-	board = [ [' ' for x in range(0,x0+2*radius+1)] for x in range(0,y0+2*radius+1)]
+	board = [ [' ' for x in range(0,x0+int(2*radius)+1)] for x in range(0,y0+int(2*radius)+1)]
 	x0 +=radius
 	y0 +=radius
-	y = 0
+	y = 1
 	x = radius
 	RE = 1-x
 	while x>=y:
-
-		board[y+y0][x+x0] = '#'
-		board[x+y0][y+x0] = '#'
-		board[-x+y0][y+x0] = '#'
-		board[-y+y0][x+x0] = '#'
-		board[-x+y0][-y+x0] = '#'
-		board[-y+y0][-x+x0] = '#'
-		board[x+y0][-y+x0] = '#'
-		board[y+y0][-x+x0] = '#'
+		a =raw_input(':')
+		print_board(board)
+		board[int(y+y0)][int(x+x0)] = '1'
+		board[int(x+y0)][int(y+x0)] = '2'
+		board[int(-x+y0)][int(y+x0)] = '3'
+		board[int(-y+y0)][int(x+x0)] = '4'
+		board[int(-x+y0)][int(-y+x0)] = '5'
+		board[int(-y+y0)][int(-x+x0)] = '6'
+		board[int(x+y0)][int(-y+x0)] = '7'
+		board[int(y+y0)][int(-x+x0)] = '8'
 		y += 1
 		if RE < 0:
 			RE += 2*y +1
 		else:
 			x -= 1
 			RE = 2*(y-x+1)
+	print_board(board)
 
+''' Drawing Ellipse'''
+def draw_ellipse(da,db):
+	'''
+		(x/a)^2 + (y/b)^2 = 1
+
+		a = da/2
+		b = db/2
+
+	'''
+
+	board = [ ['_' for x in range(0,da)] for x in range(0,db)]
+
+	wb = db-1
+	wa = da-1
+	board[0][wa>>1] = '1'
+	board[wb][wa>>1] = '2'
+	board[0][wa-(wa>>1)] = '3'
+	board[wb][wa-(wa>>1)] = '4'
+
+	board[wb>>1][0] = '5'
+	board[wb-(wb>>1)][wa] = '6'
+	board[wb>>1][wa] = '7'
+	board[wb-(wb>>1)][0] = '8'
+
+	for x in range(wa-(wa>>1),da):
+		print x+1
+	print '#'*50
+	for y in range(wb-(wb>>1),db):
+		print y+1
 
 	print_board(board)
+
 
 def print_board(board):
 	print '+'+'-'*len(board[0])+'+'
 	for index,row in enumerate(board):
-		print '|'+''.join(row)+'|'+str(index)
+		print '|'+''.join(row)+'|'+str(index+1)
 	print '+'+'-'*len(board[0])+'+'
-	print ' '+''.join([str(x%10) for x in range(0,len(board[0]))])
+	print ' '+''.join([str(x%10) for x in range(1,len(board[0])+1)])
 
 #draw_line(1,3,20,20)
-draw_circle(15,0,0)
+#draw_circle(16,0,0)
+draw_ellipse(20,20)
