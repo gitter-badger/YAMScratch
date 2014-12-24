9dcdc8be74471cd61ac08867278186529fbbac65
Another Minion <yetanotherminion@gmail.com>
2014-12-24 12:37:03 -0500

    artifacts still present when dz/dx < 1

--- matlab-misc/make_step.m
+++ matlab-misc/make_step.m
@@ -1,23 +1,38 @@
-function [x_list,y_list] = make_step(x_list,y_list,A,B)
+function [x_list,y_list,z_list] = make_step(x_list,y_list,z_list,A,B,C)
 	x = x_list(end);
 	y = y_list(end);
-	x_step = abs(B^2*((x-2)^2) + A^2*(y^2) - A^2*B^2);
-	y_step = abs(B^2*(x^2) + A^2*((y+2)^2) - A^2*B^2);
-	both = abs(B^2*((x-2)^2) + A^2*((y+2)^2) - A^2*B^2);
-	alll = [x_step,y_step,both];
-	if x_step == min(alll)
-		x = x - 2;
-		%try again to make a
-		x_list(end+1) = x;
-		y_list(end+1) = y;
-		[x_list,y_list] = make_step(x_list,y_list,A,B);
-	elseif y_step == min(alll)
-		y = y+2;
-		y_list(end+1) = y;
-		x_list(end+1) = x;
-	else
+	z = z_list(end);
+
+	x_step = (B^2*C^2*((x-2)^2) + A^2*C^2*(y^2) + A^2*B^2*(z^2) - A^2*B^2*C^2);
+	y_step = (B^2*C^2*(x^2)+ A^2*C^2*((y+2)^2) + A^2*B^2*(z^2) - A^2*B^2*C^2);
+	both = (B^2*C^2*((x-2)^2) + A^2*C^2*((y+2)^2) + A^2*B^2*(z^2) - A^2*B^2*C^2);
+
+
+	alll = [abs(x_step),abs(y_step),abs(both)];
+	if z > 18
+		disp('################')
+		disp([x,y,z])
+		disp([x_step,y_step,both])
+		disp('&&&&&&&&&&&&&&&&')
+	end
+	if abs(x_step) == min(alll)
 		x_list(end+1) = x-2;
-		y_list(end+1) = y+2;	
+		y_list(end+1) = y;
+		z_list(end+1) = z;
+		disp('made x_step')
+		%try again to make a x-step
+		%[x_list,y_list,z_list] = make_step(x_list,y_list,z_list,A,B,C);
+	elseif abs(y_step) == min(alll)
+		y_list(end+1) = y+2;
+		x_list(end+1) = x;
+		z_list(end+1) = z;
+		disp('made y_step')
+	else
+		%make both steps
+		x_list(end+1) = x-2;
+		y_list(end+1) = y+2;
+		z_list(end+1) = z;
+		disp('made both')	
 	end
 	return
 end
--- matlab-misc/voxel_ellipsoid.m
+++ matlab-misc/voxel_ellipsoid.m
@@ -3,8 +3,8 @@ clc
 close all
 
 A = 31; %x axis width
-B = 51; %y axis width
-C = 31; %z axis width
+B = 31; %y axis width
+C = 51; %z axis width
 
 x_offset = mod(A+1,2); %set the coordinate we start from
 y_offset = mod(B+1,2);
@@ -59,48 +59,57 @@ end
 % XY
 xy_x_list = [A-1];
 xy_y_list = [y_offset];
+xy_z_list = [z_offset];
 while xy_y_list(end) <= B-2
-	[xy_x_list,xy_y_list] = make_step(xy_x_list,xy_y_list,A,B);
+	[xy_x_list,xy_y_list,xy_z_list] = make_step(xy_x_list,xy_y_list,xy_z_list,A,B,C);
 	%there may still be some x error
 end
 %burn down any remaining x coordinate
 while xy_x_list(end) > 1
 		xy_x_list(end+1) = xy_x_list(end)- 2;
 		xy_y_list(end+1) = xy_y_list(end);
+		xy_z_list(end+1) = xy_z_list(end);
 end
 %END XY
 % START XZ
 xz_x_list = [A-1];
 xz_z_list = [z_offset];
+xz_y_list = [y_offset];
 while xz_z_list(end) <= C-2
-	[xz_x_list,xz_z_list] = make_step(xz_x_list,xz_z_list,A,C);
+	[xz_x_list,xz_z_list,xz_y_list] = make_step(xz_x_list,xz_z_list,xz_y_list,A,C,B);
 	%there may still be some x error
 end
 %burn down any remaining x coordinate
 while xz_x_list(end) > 1
 		xz_x_list(end+1) = xz_x_list(end)- 2;
 		xz_z_list(end+1) = xz_z_list(end);
+		xz_y_list(end+1) = xz_y_list(end);
 end
 %END XZ
 %START YZ
 yz_y_list = [B-1];
 yz_z_list = [z_offset];
+yz_x_list = [x_offset];
 while yz_z_list(end) <= C-2
-	[yz_y_list,yz_z_list] = make_step(yz_y_list,yz_z_list,B,C);
+	[yz_y_list,yz_z_list,yz_x_list] = make_step(yz_y_list,yz_z_list,yz_x_list,B,C,A);
 	%there may still be some x error
 end
 %burn down any remaining x coordinate
 while yz_y_list(end) > 1
 		yz_y_list(end+1) = yz_y_list(end)- 2;
 		yz_z_list(end+1) = yz_z_list(end);
+		yz_x_list(end+1) = yz_x_list(end);
 end
 %END YZ
 %%Calculate the A and B for each z value
 level = [];
 z_values = [];
 for z = [z_offset:2:C-1]
-	index = find(xz_z_list==z);
-	level(end+1,:) = [xz_x_list(index(1))+1,yz_y_list(index(1))+1];
+	x_index = find(xz_z_list==z);
+	level(end+1,1) = xz_x_list(x_index(1))+1;
+	%find the largest y value 
+	y_index = find(yz_z_list==z);
+	level(end,2) = yz_y_list(y_index(1))+1;
 	z_values(end+1) = z;
 end
 
@@ -112,17 +121,21 @@ z_list = [];
 for index = 1:length(z_values)
 	calc_A = level(index,1);
 	calc_B = level(index,2);
-
 	x_list(end+1) = [calc_A-1];
 	y_list(end+1) = [y_offset];
-	while y_list(end) <= calc_B-2
-		[x_list,y_list] = make_step(x_list,y_list,calc_A,calc_B);
+	z_list(end+1) = z_values(index);
+	while y_list(end) < calc_B-2
+		[x_list,y_list,z_list] = make_step(x_list,y_list,z_list,A,B,C);
 		%there may still be some x error
 	end
 	%burn down any remaining x coordinate
 	while x_list(end) > 1
+			disp('needed')
+			disp(x_list(end))
+			disp(y_list(end))
 			x_list(end+1) = x_list(end)- 2;
 			y_list(end+1) = y_list(end);
+			z_list(end+1) = z_list(end);
 	end
 	while length(z_list) < length(x_list)
 		z_list(end+1) = z_values(index);
