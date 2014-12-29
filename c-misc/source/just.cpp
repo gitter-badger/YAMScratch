231accdd23b2f230c9dcaf78c98d2b5966c79388
YetAnotherMinion <yetanotherminion@gmail.com>
2014-12-28 19:22:02 -0500

    updated gitignore

--- c-misc/source/just.cpp
+++ c-misc/source/just.cpp
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <string>
 #include <fstream>
+
 #include "ellipse.h"
 
 using namespace std;
@@ -36,6 +37,21 @@ int main(int argc, char *argv[])
 		int width = atoi(argv[1]); //careful of overflow
 		string input_file(argv[2]);
 		string output_file(argv[3]);
+
+		ifstream in_str(input_file.c_str());
+
+		ofstream out_str(output_file.c_str());
+
+		string buffer;
+
+		while(!in_str.eof())
+		{
+			buffer = in_str.getline();
+			//cout << buffer << endl;
+			out_str << buffer << "          " << buffer.length() << endl;
+			buffer = ""; //clear the string
+
+		}
 		
 		return 0;
 	}
