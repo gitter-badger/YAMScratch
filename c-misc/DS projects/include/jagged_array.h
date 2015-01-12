b88a79226130979a85e7b5299ba0116f0730b373
YetAnotherMinion <yetanotherminion@gmail.com>
2015-01-11 23:13:51 -0500

    unpack added as well as packed print

--- c-misc/DS projects/include/jagged_array.h
+++ c-misc/DS projects/include/jagged_array.h
@@ -105,7 +105,7 @@ template<typename T> unsigned int JaggedArray<T>::numElementsInBin(unsigned int 
         if(bin < numBins_) {
             //compute how many elements in that bin
             if(bin == numBins_ -1) {
-                return numBins_ - offsets_[bin];
+                return numBins_  -1 - offsets_[bin];
             } else {
                 return offsets_[bin+1] - offsets_[bin];
             }
@@ -118,14 +118,45 @@ template<typename T> unsigned int JaggedArray<T>::numElementsInBin(unsigned int 
 //Packing and Unpacking
 template<typename T> void JaggedArray<T>::pack() {
     //make sure that there are elements to allocate
-    if(numElements_) {
-        packed_values_ = new T[numElements_];
+    if(!isPacked_) {
+        if(numElements_) {
+            //allocate new arrays to update
+            packed_values_ = new T[numElements_];
+            offsets_ = new int[numBins_];
+            if(packed_values_ == NULL || offsets_ == NULL) {NULL_POINTER_EXCEPTION; exit(1); }
+            //filling pointer to packed values
+            T* fill_ptr; fill_ptr = packed_values_;
+            //first offset is always zero
+            offsets_[0] = 0;
+            for(unsigned int i = 0; i < numBins_; i++){
+                //fill in each bin
+                if(i < numBins_ -1) {
+                    offsets_[i+1] = offsets_[i] + counts_[i];
+                } else { //only case for last element
+                    offsets_[i+1] = offsets_[i] + counts_[i];
+                }
+                //copy over each of the values
+                for(unsigned int j = 0; j < counts_[i]; j++) {
+                    *fill_ptr++ = unpacked_values_[i][j];
+                }
+            }
 
-    } else {
-        offsets_ = NULL;
-
-
-    }
+        } else {
+            offsets_ = new int[numBins_];
+            //set all elements to zero
+            for(unsigned int i = 0; i < numBins_; i ++) {
+                offsets_[i] = 0;
+            }
+            packed_values_ = NULL;
+        }
+        //release the unpacked values
+        delete [] counts_;
+        delete [] unpacked_values_;
+        counts_ = NULL;
+        unpacked_values_ = NULL;
+        //change status
+        isPacked_ = true;
+    } //otherwise nothing else to do
 }
 
 template<typename T> void JaggedArray<T>::unpack() {
@@ -212,26 +243,27 @@ template<typename T> void JaggedArray<T>::removeElement(unsigned int bin, unsign
                 unpacked_values_[bin] = NULL;
                 counts_[bin] = 0;
                 numElements_ -= 1;
-                return;
+                return; //leaving function
+            } else {
+                //create a temp to hold new
+                T* temp = new T[(counts_[bin]-1)];
+                if(temp == NULL) { NULL_POINTER_EXCEPTION; exit(1); } //checking for null
+                //walk up list and copy over
+                for(unsigned int i = 0; i < slot; i++) {
+                    temp[i] = unpacked_values_[bin][i];
+                }
+                //now copy over the remaining
+                for(unsigned int i = slot+1; i < counts_[bin]; i++) {
+                    temp[i-1] = unpacked_values_[bin][i];
+                }
+                //clean up the old array
+                delete [] unpacked_values_[bin];
+                //swap in the new array
+                unpacked_values_[bin] = temp;
+                //update the number of elements
+                counts_[bin] -= 1;
+                numElements_ -= 1;
             }
-            //create a temp to hold new
-            T* temp = new T[(counts_[bin]-1)];
-            if(temp == NULL) { NULL_POINTER_EXCEPTION; exit(1); } //checking for null
-            //walk up list and copy over
-            for(unsigned int i = 0; i < slot; i++) {
-                temp[i] = unpacked_values_[bin][i];
-            }
-            //now copy over the remaining
-            for(unsigned int i = slot+1; i < counts_[bin]; i++) {
-                temp[i-1] = unpacked_values_[bin][i];
-            }
-            //clean up the old array
-            delete [] unpacked_values_[bin];
-            //swap in the new array
-            unpacked_values_[bin] = temp;
-            //update the number of elements
-            counts_[bin] -= 1;
-            numElements_ -= 1;
         } else {SLOT_INDEX_ERROR; exit(1);}
     } else { BIN_INDEX_ERROR(bin,this->numBins_); exit(1);}
 }
@@ -283,7 +315,16 @@ template<typename T> void JaggedArray<T>::print() {
     // find the max field width for num of slots in bin
     int mfw = (numElements_ /10 )+2;
     if(isPacked_) { //easy to represent
-
+        std::cout << std::setw(10) << std::setfill(FILL) << std::left << "offsets: ";
+        for(unsigned int i = 0; i < numBins_; i++) {
+            std::cout << offsets_[i] << FILL;
+        }
+        std::cout << std::endl;
+        std::cout << std::setw(10) << std::left << "packed: ";
+        for(unsigned int i = 0; i < numElements_ ; i++) {
+            std::cout << packed_values_[i] << FILL;
+        }
+        std::cout << std::endl;
     } else {
         // determine the max number of slots in a bin as we display them
         //we only have to call setfill once in this case
