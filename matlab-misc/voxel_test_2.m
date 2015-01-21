27157150a0e755f59eb70858a7c56e86ba9a7473
Another Minion <yetanotherminion@gmail.com>
2015-01-21 15:14:30 -0500

    added more commits

--- matlab-misc/voxel_test_2.m
+++ matlab-misc/voxel_test_2.m
@@ -2,9 +2,9 @@ clear
 clc
 close all;
 
-A = 40;
-B = 3;
-C = 30;
+A = 2^32-1;
+B = 2^32-1;
+C = 2^32-1;
 
 x_offset = mod(A+1,2); %set the coordinate we start from
 y_offset = mod(B+1,2);
@@ -41,6 +41,10 @@ for k = z_offset:2:C
     x = begining_x;
     disp('Finding Error')
     error = (B^2*x^2 + A^2*y^2 - A^2*B^2)*C^2 + A^2*B^2*z^2;
+    if error > (2^64-1)
+        disp(error);
+        return;
+    end
     disp(error)
     %if the error is greater than zero than decrease x until it is less
     %than zero
@@ -90,16 +94,16 @@ for k = z_offset:2:C
         %determine which one will allow us to be closest to negative zero
         if key == x_step
             x = x-2;
-            disp('making x step');
+            %disp('making x step');
         elseif key == xy_step
             x = x-2;
             y = y+2;
-            disp('making xy step');
+            %disp('making xy step');
 
         else
             %it is a y step
             y = y+2;
-            disp('making y step');
+            %disp('making y step');
 
         end
         error = key;
@@ -112,27 +116,33 @@ for k = z_offset:2:C
     
 end
 
-for count = 1:length(x_list)
-	x = x_list(count);
-	y = y_list(count);
-	z = z_list(count);
-	%XY plane
-	one_x = [x-1,x-1,x+1,x+1];
-	one_y = [y+1,y-1,y-1,y+1];
-	one_z = [z+1,z+1,z+1,z+1];
-	fill3(one_x,one_y,one_z,'c')
-	fill3(one_x,one_y,-one_z,'c')
-	%XZ plane
-	two_x = [x-1,x-1,x+1,x+1];
-	two_y = [y+1,y+1,y+1,y+1];
-	two_z = [z+1,z-1,z-1,z+1];
-	fill3(two_x,two_y,two_z,'m')
-	fill3(two_x,two_y,-two_z,'m')
-	%YZ plane
-	three_x = [x+1,x+1,x+1,x+1];
-	three_y = [y-1,y-1,y+1,y+1];
-	three_z = [z+1,z-1,z-1,z+1];
-	fill3(three_x,three_y,three_z,'y')
-	fill3(three_x,three_y,-three_z,'y')
-
-end
+% for count = 1:length(x_list)
+% 	x = x_list(count);
+% 	y = y_list(count);
+% 	z = z_list(count);
+% 	%XY plane
+% 	one_x = [x-1,x-1,x+1,x+1];
+% 	one_y = [y+1,y-1,y-1,y+1];
+% 	one_z = [z+1,z+1,z+1,z+1];
+% 	fill3(one_x,one_y,one_z,'c')
+% 	fill3(one_x,one_y,-one_z,'c')
+% 	fill3(-one_x,one_y,one_z,'c')
+% 	fill3(-one_x,one_y,-one_z,'c')
+% 	%XZ plane
+% 	two_x = [x-1,x-1,x+1,x+1];
+% 	two_y = [y+1,y+1,y+1,y+1];
+% 	two_z = [z+1,z-1,z-1,z+1];
+% 	fill3(two_x,two_y,two_z,'m')
+% 	fill3(two_x,two_y,-two_z,'m')
+%     fill3(-two_x,two_y,two_z,'m')
+% 	fill3(-two_x,two_y,-two_z,'m')
+% 	%YZ plane
+% 	three_x = [x+1,x+1,x+1,x+1];
+% 	three_y = [y-1,y-1,y+1,y+1];
+% 	three_z = [z+1,z-1,z-1,z+1];
+% 	fill3(three_x,three_y,three_z,'y')
+% 	fill3(three_x,three_y,-three_z,'y')
+%     fill3(-three_x,three_y,three_z,'y')
+% 	fill3(-three_x,three_y,-three_z,'y')
+% 
+% end
