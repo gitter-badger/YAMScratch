01a543f32ae148a659bee28d6b1a4c0f43c48f42
Another Minion <yetanotherminion@gmail.com>
2015-02-02 22:05:38 -0500

    temperature init

--- matlab-misc/SimAnneal.m
+++ matlab-misc/SimAnneal.m
@@ -18,35 +18,77 @@ close all;
 fid = fopen('results.txt','at');
 
 %randomly pick a intial starting point on interval [-1000,1000]
-X = rand('double')*2000 - 1000;
+U = 1000;
+L = -U;
+x_k = rand('double')*(U-L) - U;
 fprintf(fid, 'Starting x: %f\n', X );
+fprintf(fid, 'x, f(x), g(x)\n' ); %tell the format
 
-
-
-
-N = 100; %size of the inital value array 
+N = 1; %
 epoch_length = 2*N; %twice
 
+mesh_size = (U-L) /5; %initialize the mesh size for a jump of 1/5 interval
 
-
-mesh_size = 0.1; %should be greater than zero
-trial_radius = 3; % SA trial point radius
-tau = 1.1; % must be greater than 1
-omega_neg = -1; % must be less than or equal to -1
-omega_pos = 1; % must be greater or equal to 1
 epsilon = 0.001; %neighborhood radius
+trial_radius = epsilon; % SA trial point radius
 
-T_max = func1(x_0);
+
+x_nearby = x_k + (rand*2 -1) *trial_radius;
+
+[f_x_k, g_x_k] = func1(x_k);
+fprintf(fid, 'Starting: %10.10f,%10.10f,%10.10f\n',[x_k, f_x_k, g_x_k] );
+[f_near, g_near] = func1(x_nearby);
+fprintf(fid, 'Nearby: %10.10f,%10.10f,%10.10f\n',[x_nearby, f_near, g_near] );
+
+T_max = -abs(f_near - f_x_k) / log(0.9);
 T_min = min([1,T_max]*0.001);
 T_init = T_max;
+fprintf(fid, 'T_max: %10.10f \n T_min: %10.10f\n', [T_max,T_min]);
+
+memo = false; % flag to skip recalculating first time through loop
 
 cool_ratio = 0.98; %should be (0.5,0.99)
 
 
-for search = 1:m
+for search = 1:epoch_length
+	%log which iteration we are on
+	fprintf(fid, 'Iteration: %i \n', [search] );
+	%evaluate the test point x_k
+	if(memo)
+		[f_x_k, g_x_k] = func1(x_k);
+		fprintf(fid, '%10.10f,%10.10f,%10.10f\n',[x_k, f_x_k, g_x_k] );
+		memo = true; 
+	end
+	
+	%generate an exploring point z_k
+	z_k = rand * epsilon + x_k;
+	%evaulate f(z_k)
+	[f_z_k, g_z_k] = func1(z_k);
+	fprintf(fid, '%10.10f,%10.10f,%10.10f\n',[z_k, f_z_k, g_z_k] );
+	nu = 0.1 + (1-0.1) * rand;
+	if (f_z_k <= f_x_k)
+		%the expression is simplified by the fact that x is a one dimensional vector
+		x_SA = x_k + nu*trial_radius * ((z_k - x_k)/abs(z_k-x_k));
+	else 
+		x_SA = x_k - nu*trial_radius * ((z_k - x_k)/abs(z_k-x_k));
+	end
+	%evaluate f(x_SA) and accept it conditionally
+	[f_x_SA, g_x_SA] = func1(x_SA);
+	fprintf(fid, '%10.10f,%10.10f,%10.10f\n\n',[x_SA, f_x_SA, g_x_SA] );
+	if(f_x_SA - f_x_k < 0)
 
+	elseif ()
+		
+	else % do local PS
+		if (f_prime_x == 0)
+			%return, we have found a minimum
+			break;
+		%test using the derivative
+		else
+			if
 
-	fprintf(fid,'%f %f',[f_x, f_prime_x]); %writing the value
+		end
+	end
 end
 
 fclose(fid);
