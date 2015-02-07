284da05df987a3e7d735ce4913359604bc41cd01
Another Minion <yetanotherminion@gmail.com>
2015-02-07 17:13:34 -0500

    variable arguments
    test the function inputs

--- matlab-misc/GradientCalc.m
+++ matlab-misc/GradientCalc.m
@@ -1,5 +1,12 @@
-clear
-clc
-close 'all'
+function [] = GradientCalc(fhandle,varargin)
+	p = inputParser;
+	defaultStep = ones(128,1);
+	check_fun = @(x)(isa(x,'function_handle'));
+	%load the requirements
+	addRequired(p,'fhandle',check_fun);
+	%run the parser on the inputs
+	parse(p,fhandle,varargin{:});
 
-func = 
+	return
+end
+
