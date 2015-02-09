2dcd5cce331b9dacba81c46cc7a11add7d288674
Another Minion <yetanotherminion@gmail.com>
2015-02-09 15:37:44 -0500

    more default arguments and checks

--- matlab-misc/GradientCalc.m
+++ matlab-misc/GradientCalc.m
@@ -1,12 +1,21 @@
 function [] = GradientCalc(fhandle,varargin)
 	p = inputParser;
-	defaultStep = ones(128,1);
+	VECTOR_SIZE = 128; 
+	defaultStep = ones(VECTOR_SIZE,1);
+	defaultLocation = zeros(VECTOR_SIZE,1);
+	 
 	check_fun = @(x)(isa(x,'function_handle'));
+	function [result] = validSize(Matrix)
+		[r,c] = size(Matrix);
+		result = (r == VECTOR_SIZE || c == VECTOR_SIZE ) && isvector(Matrix);
+	end
 	%load the requirements
 	addRequired(p,'fhandle',check_fun);
+	addOptional(p,'step',defaultStep,validSize);
+	addOptional(p,'location',defaultLocation,validSize);
 	%run the parser on the inputs
 	parse(p,fhandle,varargin{:});
+	disp(p.Results)
 
 	return
 end
-
