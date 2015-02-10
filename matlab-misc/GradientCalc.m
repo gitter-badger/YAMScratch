477fd06710c403a8473f15e13d39bff178f049cc
Another Minion <yetanotherminion@gmail.com>
2015-02-10 13:29:26 -0500

    fixed input parsing
    the optional input needed to be passed a function handle also move
    function declaration outside of primary function definition for style

--- matlab-misc/GradientCalc.m
+++ matlab-misc/GradientCalc.m
@@ -5,17 +5,18 @@ function [] = GradientCalc(fhandle,varargin)
 	defaultLocation = zeros(VECTOR_SIZE,1);
 	 
 	check_fun = @(x)(isa(x,'function_handle'));
-	function [result] = validSize(Matrix)
-		[r,c] = size(Matrix);
-		result = (r == VECTOR_SIZE || c == VECTOR_SIZE ) && isvector(Matrix);
-	end
 	%load the requirements
 	addRequired(p,'fhandle',check_fun);
-	addOptional(p,'step',defaultStep,validSize);
-	addOptional(p,'location',defaultLocation,validSize);
+	addOptional(p,'step',defaultStep,@validSize);
+	addOptional(p,'location',defaultLocation,@validSize);
 	%run the parser on the inputs
 	parse(p,fhandle,varargin{:});
 	disp(p.Results)
 
 	return
+end
+
+function [result] = validSize(Matrix)
+		[r,c] = size(Matrix);
+		result = (r == VECTOR_SIZE || c == VECTOR_SIZE ) && isvector(Matrix);
 end
