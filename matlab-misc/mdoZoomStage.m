9f7d9fa54bf5da0ab3790b92b2585fbfb04f450f
Another Minion <yetanotherminion@gmail.com>
2015-02-10 22:01:34 -0500

    started zoom stage

--- matlab-misc/mdoZoomStage.m
+++ matlab-misc/mdoZoomStage.m
@@ -1,12 +1,24 @@
-function [alpha] = mdoZoomStage(al_low,al_high,f_low,f_high)
+function [alpha] = mdoZoomStage(phi,al_low,al_high,boundary)
 %Purpose: returns
 % al_low - the step size for which phi is smaller
 % al_high - the step size for which phi is larger
-% f_low - cached function evaluation
-% f_high - cached function evaluation
+% boundary - a struct containing fields listed below
+%		f_low - vector corresponding to phi(al_low)
+%		g_low - vector corresponding to phi'(al_low)  is possible to be NaN
+%		f_high - vector corresponding to phi(al_high)
+%		g_high - vector corresponding to phi(al_high)
+%		f_init - vector corresponding to phi(0)
+%		g_init - vector corresponding to phi'(0)
+disp(boundary_conditions)
+	while true
+		%find a trial point between al_low and al_high
+		%this uses bisection
+		if 
+		%now zoom into the interval
+		[f_this, g_this] = phi(alpha_this);
 
-	%find a trial point between al_low and al_high
-	%
-
-	return
-end
+		return
+		%
+	end
+		return
+end	
