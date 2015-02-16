defb2b2735b8485f7d09260d6f561e7a7bfaf6e2
Another Minion <yetanotherminion@gmail.com>
2015-02-15 21:50:51 -0500

    saved a function evaluation

--- matlab-misc/mdoBlackBox3.m
+++ matlab-misc/mdoBlackBox3.m
@@ -2,10 +2,12 @@ clear('all');
 clc
 close all;
 %configure the linesearch parameters here
-x_prev = ones(128,1);
+x_prev = ones(128,1)*-5;
+x_prev(37) = 3;
+x_prev(75) = 0;
 mu_1 = 1e-4;
 mu_2 = 0.9;
-alpha_init = .9;
+alpha_init = 1;
 alpha_max = 3;
 num_evals = mdoCounter;
 obj = mdoEvalCounter(@func3,num_evals);
@@ -15,10 +17,11 @@ obj = mdoEvalCounter(@func3,num_evals);
 g_metric = [];
 x_metric(1,:) = [x_prev];
 %do a bad thing and have infinite while loop to simulate do while
-[f_curr,g_curr] = obj(x_prev);
+[f_curr,g_curr] = deal(f_init,g_init);
 format_grad_norm = '\t[f, norm(g)] = %10.5g , %10.5g \n';
 format_func_evals = '\tTotal function evaluations: %d \n';
 iteration = 1;
+func_cost = [];
 while true
 	%compute the descent direction of the objective function
 	%in this case we directly evaluate the gradient and then normalize
@@ -26,6 +29,7 @@ while true
 	fprintf(format_grad_norm, f_curr, norm(g_curr));
 	output_count = num_evals.count;
 	fprintf(format_func_evals,output_count );
+	func_cost(end+1) = output_count;
 	p = -g_curr./norm(g_curr);
 	step = mdoLineSearch(obj, p, x_prev, mu_1, mu_2, alpha_init, alpha_max);
 	%jump to this point
