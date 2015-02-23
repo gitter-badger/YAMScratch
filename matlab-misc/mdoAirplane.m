8cef80b00bb69fcb5d146db25e1adef0fbc3e059
Another Minion <yetanotherminion@gmail.com>
2015-02-23 14:03:40 -0500

    abstract base class for airplane problems

---
+++ matlab-misc/mdoAirplane.m
@@ -0,0 +1,18 @@
+classdef AirPlane
+	properties
+		beam;
+		chord;
+		W_0;
+		N_ult;
+		t_over_c;
+		rho;
+		mu;
+		Velocity;
+		Surf_area;	%
+		Surf_wet;	%wing wetted area
+		k;			%form factor
+		Co_lift;	%Coefficient of Lift
+		e; 			%Oswald efficiency factor
+		
+	end
+
