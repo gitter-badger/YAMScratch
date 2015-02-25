b97609dc2b20e9123e0a9987b8b4cfb37fa48435
Another Minion <yetanotherminion@gmail.com>
2015-02-24 21:01:23 -0500

    changed name to match

---
+++ matlab-misc/+mdo/AirPlane.m
@@ -0,0 +1,31 @@
+classdef AirPlane
+	properties(Constant)
+		W_0 = 4940;			%Initial weight in newtons
+		N_ult = 2.5;		%
+		t_over_c  = 0.12;	%
+		rho = 1.23;			%density of air
+		mu = 17.8e-6;		%viscosity of air
+		Velocity = 35;		%airspeed
+		k = 1.2;			%form factor
+		e = 0.96; 			%Oswald efficiency factor
+	end
+	properties
+		S_area_wet;
+		S_area;
+		beam;
+		chord;
+	end
+	methods
+
+	end
+	methods
+		function obj = AirPlane(A,S)
+			assert(isscalar(S));
+			assert(isscalar(A));
+			obj.S_area = S;
+			obj.S_area_wet = 2.05 * S;
+			obj.beam = sqrt(A*S);
+			obj.chord = sqrt(S/A);
+		end
+	end
+end
