535e5ab47294e4f0ed1d1edbc433e73759858391
YetAnotherMinion <yetanotherminion@gmail.com>
2015-03-03 22:46:15 -0500

    debug output

--- python-misc/slicer/slicer.py
+++ python-misc/slicer/slicer.py
@@ -207,6 +207,19 @@ if __name__ == '__main__':
 		print value.faces
 		print value.edges, '\n'
 
+	print '====================================='
+	for value in mesh.edges.values():
+		assert(len(value.faces) == 2)
+		print value.faces
+		print value.vertices, '\n'
+
+	print '====================================='
+	for value in mesh.faces.values():
+		assert(len(value.edges) == 3)
+		assert(len(value.vertices) == 3)
+		print value.edges
+		print value.vertices
+
 	# dwg = ezdxf.new("AC1015")
 	# msp = dwg.modelspace()
 
