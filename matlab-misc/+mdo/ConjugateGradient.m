33010f8d3c8c86fc4a9030c049e803188d3af2d9
Another Minion <yetanotherminion@gmail.com>
2015-03-18 13:28:17 -0400

    CG works sort of now

--- matlab-misc/+mdo/ConjugateGradient.m
+++ matlab-misc/+mdo/ConjugateGradient.m
@@ -1,5 +1,5 @@
 %% ConjugateGradient: function description
-function [x_star, history] = ConjugateGradient(obj, grad, LineSearch, x_0, e_g, e_a, e_r)
+function [x_star, logObj] = ConjugateGradient(obj, grad, linesearch, x0, e_g, e_a, e_r, logObj)
 %Purpose: returns the location of a local minimum and a history object
 %Inputs:
 %	obj - a function handle for the objective
@@ -9,33 +9,89 @@ function [x_star, history] = ConjugateGradient(obj, grad, LineSearch, x_0, e_g, 
 %	e_g - epsilon tolerance for the gradient
 % 	e_a - epsilon tolerance for the absolute
 %	e_r - epsilon tolerance for the scale factor
-
-	k = 0;
-	x_prev = x_0;
-	x_curr = x_0;
-	%start out by computing the steepest descent direction
 	
-	p_curr
-	while True
-		%we skip this round on the begining
-		if k ~= 0
-			B = (g_curr.' * g_curr)/(g_prev.' * g_prev);
-			p_curr = -g_curr + B*p_prev;
+	%sanity checks
+	assert(isvector(x0))
+
+	mu_1 = 1e-4;
+	mu_2 = 0.9;
+	max_iter = 500;
+	alpha_init = 1;
+	alpha_max = 5;
+	f_prev = obj(x0);
+	gk = grad(x0);
+	%store the very first gradient in local memory
+	grad_init = gk;
+	%start off
+	xk = x0;
+	%log the very first time
+	k = 1;
+	logObj.editIteration(k, f_prev, xk, gk, 1, 1);
+	x_prev = x0;
+	xk = x0;
+	
+	%figure out the size of the input vector to determine how often to restart
+	[r,c] = size(x0);
+	N = max(r,c);
+
+	while true
+		%we reset every N iterations using steepest descent
+		if mod(k, N) == 1
+			%start out by computing the steepest descent direction
+			gk = grad(xk);
+			pk = -gk./norm(gk);
+			extra_grad_eval = 1;
+		else
+			B = (gk.' * gk)/(g_prev.' * g_prev);
+			pk = -gk + B*p_prev;
+			extra_grad_eval = 0;
+			%there is a wierd bug where this is not a descent direction
+			if( (gk.' * pk) > 0)
+				%so we force it to be a descent direction
+				pk = - gk./norm(gk);
+			end
 		end
 		%perform a line search
-		step = LineSearch
-		%add the information to the history object
+		[step, fnew, num_f_evals, num_df_evals] = linesearch(obj, grad, xk, pk, ...
+		 	mu_1, mu_2, alpha_init, alpha_max, max_iter);
+		%update for next iteration and log
+		k = k + 1;
+		p_prev = pk;
+		x_prev = xk;
+		g_prev = gk;
+		%update the x position and gradient
+		xk = xk + step*pk;
+		gk = grad(xk);
 
-		%update the memoized data
-		p_prev = p_curr;
-		x_prev = x_curr;
-		g_prev = g_curr;
+		logObj.editIteration(k, fnew, xk, gk, num_f_evals, num_df_evals+1 + extra_grad_eval);
 
-		if abs(f_curr - f_prev) <= (e_a + e_r*abs(f_prev) and norm(g_prev) <= e_g
-			break
+		%sprintf('Loop iteration %d', k)
+		%This condition from the slide and scales input
+			% deltaf = abs(fnew - f_prev);
+			% if deltaf <= (e_a + e_r * abs(f_prev))
+			% 	if norm(g_prev) <= e_g
+			% 		r = true;
+			% 	else
+			% 		r = false;
+			% 	end
+
+			% else 
+			% 	r = false;
+			% end
+		%This condition only relies on the gradient
+		if (norm(gk) < e_g * norm(grad_init))
+			r = true;
+		else
+			r = false;
 		end
+		
+		if r
+			break;
+		end
+		%update the previous f value after we use it
+		f_prev = fnew;
 	end
 
-    x_star = 0; 
+    x_star = xk; 
     return
 end
