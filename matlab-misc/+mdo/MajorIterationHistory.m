bc3047e9f7f94443b4e6e40100063e18dd46fa8f
Another Minion <yetanotherminion@gmail.com>
2015-03-17 21:42:33 -0400

    added function logging

--- matlab-misc/+mdo/MajorIterationHistory.m
+++ matlab-misc/+mdo/MajorIterationHistory.m
@@ -5,25 +5,33 @@ classdef MajorIterationHistory < handle
         total_iterations;
         x;
         g;
+        fevals
+        dfevals
     end
     methods
         function obj = MajorIterationHistory()
             obj.total_iterations = 0;
         end
-        function editIteration(obj, k, x, g)
+        function editIteration(obj, k, x, g, fevals, dfevals)
             %convert the vector to a column vector
             assert(isvector(x))
             assert(isvector(g))
+            assert(isscalar(fevals))
+            assert(isscalar(dfevals))
             if(k > obj.total_iterations)
                 obj.total_iterations = k;
             end
             obj.x(k, :) = x;
             obj.g(k, :) = g;
+            obj.fevals(k, 1) = fevals;
+            obj.dfevals(k, 1) = dfevals;
         end
-        function appendIteration(obj, x, g)
+        function appendIteration(obj, x, g, fevals, dfevals)
             obj.total_iterations = obj.total_iterations + 1;
             obj(end+1, :) = x;
             obj(end+1, :) = g;
+            obj.fevals(end+1, 1) = fevals;
+            obj.dfevals(end+1, 1) = dfevals;
         end
     end
 end
