1a64c7570b1e577c569a5c3575dcd4c899c52796
Another Minion <yetanotherminion@gmail.com>
2015-03-19 15:18:42 -0400

    adjusted parameters

--- matlab-misc/+mdo/GradientDragTotal.m
+++ matlab-misc/+mdo/GradientDragTotal.m
@@ -1,5 +1,5 @@
 function [grad] = GradientDragTotal(x)
-	cs_h = 1e-20;
+	cs_h = 1e-60;
 	%make sure we return a column vector
 	grad(1,1) = imag(mdo.DragTotal(x(1)+ 1i * cs_h, x(2))) / cs_h;
 	grad(2,1) = imag(mdo.DragTotal(x(1), x(2) + 1i * cs_h)) / cs_h;
---
+++ matlab-misc/+mdo/PlotMajorIterationConvergance.m
@@ -0,0 +1,4 @@
+function PlotMajorIterationConvergance(fig, logObj, format_spec)
+
+
+end
