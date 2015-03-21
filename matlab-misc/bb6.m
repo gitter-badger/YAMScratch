554bdfdeff5bc6ad2067780188d870b28d46fc34
Another Minion <yetanotherminion@gmail.com>
2015-03-21 18:14:00 -0400

    improved search

--- matlab-misc/bb6.m
+++ matlab-misc/bb6.m
@@ -3,29 +3,36 @@ clc
 close all;
 import mdo.*;
 
-obj = @(X,Y)( func6([X,Y].') );
-CONTOUR = false
+%create the filename to save the figure to
+d = datestr(clock);
+prefix = 'meshplot_';
+filename = regexprep(d, '\s|-|:', '_');
+mesh_filename = strcat(prefix, d);
+
+
+CONTOUR = false;
 if CONTOUR
 	a= 2;
 	b= 2;
 	grid_scale = 1e-1;
 	x = -a:grid_scale:a;
 	y = -b:grid_scale:b;
-
+	%special objective for use with array fun
+	obj = @(X,Y)( func6([X,Y].') );
 	[X,Y] = meshgrid(x,y);
 	tic
 	out = arrayfun(obj, X, Y);
 	toc
-	%create the filename to save the figure to
-	d = datestr(clock);
-	prefix = 'meshplot_';
-	filename = strcat(prefix, d);
-	filename = regexprep(filename, '\s|-|:', '_')
 
 	mesh_fig = figure;
 	mesh(x, y, out);
-	saveas(mesh_fig, filename, 'fig')
+	saveas(mesh_fig, mesh_filename, 'fig')
+	con_fig = figure;
+	contour(x,y,out,50);
+	con_name = strcat('contour', filename);
+	saveas(con_fig, con_name, 'fig');
 	disp('All done')
+	exit;
 end
 
 linesearch = @mdo.ProfLinesearch;
@@ -37,27 +44,56 @@ alpha_max = 20;
 tolerance = 1e-6;
 ls_parameters = [mu_1, mu_2, alpha_init, alpha_max, tolerance];
 %create the objective function and the gradient
-obj = @func6
-grad = @gradfunc6;
-X_0 = [1;
-		1];
+obj = @func6;
+grad = @(X)( mdo.ComplexStepGrad(obj, X, 1e-20));
+X_0 = [-.7;
+		-.9];
 
 %===============================================
 %			Steepest Descent
 %===============================================
+disp('======Starting Steepest Descent===========')
 steep_log = MajorIterationHistory();
 x_star = SteepestDescent(linesearch, obj, grad, X_0, steep_log, ls_parameters);
 %===============================================
 %			Conjugate gradient method
 %===============================================
- e_g = 1e-6;
- e_a = 1e-6;
- e_r = 1e-6;
+e_g = 1e-6;
+e_a = 1e-6;
+e_r = 1e-6;
+disp('======Conjugate Gradient===========')
 congj_log = MajorIterationHistory();
 x_star = ConjugateGradient(linesearch, obj, grad, X_0, e_g, e_a, e_r, congj_log, ls_parameters);
 %===============================================
 %			    Quasi Newton Method
 %===============================================
+disp('======Quasi Newton BFGS===========')
 qn_log = MajorIterationHistory();
 x_star = QuasiNewtonBFGS(linesearch, obj, grad, X_0, e_g, e_a, e_r, qn_log, ls_parameters);
 
+both_gradients_fig = figure;
+gradients_fig = subplot(1,2,1);
+semilogy(1,1)
+PlotMajorIterationConvergance(both_gradients_fig, steep_log, 'kd-');
+PlotMajorIterationConvergance(both_gradients_fig, congj_log, 'ro-.');
+PlotMajorIterationConvergance(both_gradients_fig, qn_log, 'b<--');
+subplot(gradients_fig);
+xlabel('Major Iterations');
+ya1 = ylabel('|g_k|/|g_0|','Rotation',0);
+set(ya1,'Units','Normalized','Position',[-0.17 0.5 0]);
+
+minor_fig = subplot(1,2,2);
+semi_hand = semilogy(1,1);
+PlotMinorIterationConvergance(both_gradients_fig, steep_log, 'kd-', minor_fig);
+PlotMinorIterationConvergance(both_gradients_fig, congj_log, 'ro-.', minor_fig);
+PlotMinorIterationConvergance(both_gradients_fig, qn_log, 'b<--', minor_fig);
+subplot(minor_fig);
+delete(semi_hand)
+xlabel('Function Evaluations')
+ya2 = ylabel('|g_k|/|g_0|','Rotation',0);
+set(ya2,'Units','Normalized','Position',[-0.17 0.5 0]);
+legend1 = legend('Steepest descent', 'Conjugate gradient', 'Quasi Newton BFGS');
+set(legend1, 'Position',[0.592125803489439 0.171707822533567 0.164370982552801 0.106246351430239]);
+
+var_filename = strcat('search_', filename);
+save(var_filename)
