705cb2ba714b26d4f7fd401a1b4cb7e5a65e0143
Another Minion <yetanotherminion@gmail.com>
2015-04-01 12:26:47 -0400

    rearranged

--- matlab-misc/+mdo/Assign4.m
+++ matlab-misc/+mdo/Assign4.m
@@ -15,15 +15,17 @@ Velocity = 35;
 
 V_min = 22;         %maximum landing velocity
 C_L_max = 2;        %maximum coefficient of lift at landing
+
+X_0 = [20,30];
 %===========================================================
 %The plane object that holds all of the methods
-plane = mdo.ComputeAirPlane(N_ult, t_over_c, W_0, rho, mu, k, e, S_wet_ratio, Velocity);
+plane1 = mdo.ComputeAirPlane(N_ult, t_over_c, W_0, rho, mu, k, e, S_wet_ratio, Velocity);
 %===========================================================
 %plot the contour and feasible region
 a = [5:1e0:30]; % A
 s = [5:1e0:40]; % S
 [AA,SS] = meshgrid(a,s);
-Drag = arrayfun(@plane.m_DragForce, AA, SS);
+Drag = arrayfun(@plane1.m_DragForce, AA, SS);
 fig_1 = figure;
 contour(AA,SS,Drag,50);
 xlabel('A');
@@ -31,7 +33,7 @@ ylab = ylabel('S','Rotation',0);
 set(ylab,'Units','Normalized','Position',[-0.07 0.5 0]);
 hold on
 
-fun = @(aspect, surface)(plane.m_LandingConstraint(aspect, surface, V_min, C_L_max));
+fun = @(aspect, surface)(plane1.m_LandingConstraint(aspect, surface, V_min, C_L_max));
 
 [scatA, scatS] = meshgrid([5:0.1:30],[5:0.1:40]);
 Constraint_out = arrayfun(fun, scatA, scatS);
@@ -40,15 +42,22 @@ scatA(Constraint_out < 0) = NaN;
 scatS(Constraint_out < 0) = NaN;
 scatter(scatA(:), scatS(:), 3, 'filled')
 
-X_0 = [20,30];
+%============================================================
+%           LOG BARRIER METHOD                              %
+%============================================================
+
+%============================================================
+%           FMINCON WITH SQP                                %
+%============================================================
+plane2 = mdo.ComputeAirPlane(N_ult, t_over_c, W_0, rho, mu, k, e, S_wet_ratio, Velocity);
+
+
 lb = [1; 1];
 ub= [inf; inf];
 options = optimoptions('fmincon', 'Algorithm', 'sqp' ,'GradObj', 'on', 'GradConstr', 'on');
-
-fmin_obj = @(X)(plane.m_DragForce(X(1), X(2)));
-
-nonlincon = @(X)(plane.m_nonLinearConstraint(X(1), X(2), V_min, C_L_max));
-
+fmin_obj = @(X)(plane2.m_DragForce(X(1), X(2)));
+nonlincon = @(X)(plane2.m_nonLinearConstraint(X(1), X(2), V_min, C_L_max));
 [x,fval, exitflag, output, lambda] = fmincon(fmin_obj,X_0,[], [], [], [], lb, ub, nonlincon, options );
+
 hold on;
 plot(x(1), x(2), 'kd');
