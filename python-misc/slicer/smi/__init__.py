1b91e90978c1992fdbb5a2f600d845b63765534a
YetAnotherMinion <yetanotherminion@gmail.com>
2015-04-02 23:11:07 -0400

    ascii functionality

--- python-misc/slicer/smi/__init__.py
+++ python-misc/slicer/smi/__init__.py
@@ -3,5 +3,4 @@ from mesh import *
 from simple_mesh import SimpleMesh
 from parsers import *
 from intersection_iterator import IntersectionGenerator
-print "here"
 from single_layer import SingleLayer
--- python-misc/slicer/smi/parsers/ascii_parser.py
+++ python-misc/slicer/smi/parsers/ascii_parser.py
@@ -1,4 +1,21 @@
 
+import smi
+import VectorMath
 
 def parseTextSTL(filename, quaternion = None, scale = 1):
-	print "not implemented"
+	for i in range(0,3):
+		try:
+			f = open(filename,'r')
+			break
+		except IOError as e:
+			print e
+			exit(1)
+	m = smi.SimpleMesh()
+	vert_view = m.vertices.viewkeys()
+	line = f.next()
+	print line
+
+	print "="*50
+	for line in f:
+		print line
+	return m
