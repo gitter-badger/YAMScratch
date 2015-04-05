a9035b80de7dd67943540b171377d353cdec107f
Another Minion <yetanotherminion@gmail.com>
2015-04-04 22:53:05 -0400

    added in mu

--- matlab-misc/+mdo/LogBarrierFactory.m
+++ matlab-misc/+mdo/LogBarrierFactory.m
@@ -1,12 +1,10 @@
 function [ff, fg] = LogBarrierFactory(planeObj, V_min, C_L_max)
-	function [phi] =logBarrier(A,S)
-		Mu = 1;
+	function [phi] =logBarrier(A, S, Mu)
 		%flip the sign on the stall constraint because it is in form c(x) <= 0
 		phi = planeObj.m_DragForce(A, S)  + Mu * (log( -planeObj.m_LandingConstraint(A, S, V_min, C_L_max)));
 	end
 
-	function [gradphi] = gradLogBarrier(A,S)
-		Mu = 1;
+	function [gradphi] = gradLogBarrier(A, S, Mu)
 		[drag,grad_drag] = planeObj.m_DragForce(A,S);
 		[cons, grad_cons] = planeObj.m_LandingConstraint(A, S, V_min, C_L_max);
 
