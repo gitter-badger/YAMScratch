b4ed49270ae30977b8a6e42dba5c2830f729944b
Another Minion <yetanotherminion@gmail.com>
2015-04-06 22:46:23 -0400

    fixed gradient bug

--- matlab-misc/+mdo/LogBarrierFactory.m
+++ matlab-misc/+mdo/LogBarrierFactory.m
@@ -7,8 +7,8 @@ function [ff, fg] = LogBarrierFactory(planeObj, V_min, C_L_max)
 	function [gradphi] = gradLogBarrier(A, S, Mu)
 		[drag,grad_drag] = planeObj.m_DragForce(A,S);
 		[cons, grad_cons] = planeObj.m_LandingConstraint(A, S, V_min, C_L_max);
-
-		gradphi = grad_drag + (Mu * 1/(-planeObj.m_LandingConstraint(A, S, V_min, C_L_max))) * grad_cons;
+		%flip the sign on the stall constraint because it is in form c(x) <= 0
+		gradphi = grad_drag + (Mu * 1/(-planeObj.m_LandingConstraint(A, S, V_min, C_L_max))) * -1 * grad_cons;
 	end
 	ff = @logBarrier;
 	fg = @gradLogBarrier;
