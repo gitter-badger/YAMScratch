7a72eb8a92b3dd7873953e4addd0e571070fb3a6
Another Minion <yetanotherminion@gmail.com>
2015-04-07 08:23:50 -0400

    getting convergence

--- matlab-misc/+mdo/Assign4.m
+++ matlab-misc/+mdo/Assign4.m
@@ -61,7 +61,7 @@ mu_1 = 1e-4;
 mu_2 = 0.9;
 alpha_init = .5;
 alpha_max = 10;
-tolerance = 0.5;
+tolerance = 1e-2;
 ls_parameters = [mu_1, mu_2, alpha_init, alpha_max, tolerance];
 e_g = 1e-6;
 e_a = 1e-6;
@@ -75,25 +75,26 @@ Beta = 0.5;
 xk = X_0;
 
 while index <= iter_max
-	figure()
+    figure()
 
-	new_obj = @(X)(obj(X(1), X(2), Mu));
-	new_grad = @(X)(grad(X(1), X(2), Mu));
-	ta = [10:5e-1:30]; % A
-	ts = [5:5e-1:40]; % S
-	[tAA,tSS] = meshgrid(ta,ts);
-	tDrag = arrayfun(obj, tAA, tSS, ones(size(tAA))*Mu);
-	contour(tAA,tSS,tDrag,50);
-	qn_log = MajorIterationHistory();
-	x_star = QuasiNewtonBFGS(linesearch, new_obj, new_grad, xk, e_g, e_a, e_r, qn_log, ls_parameters)
-	%x_star = ConjugateGradient(linesearch, new_obj, new_grad, X_0, e_g, e_a, e_r, qn_log, ls_parameters)
-	xk = x_star
-	hold on
-	plot(qn_log.x(:,1),qn_log.x(:,2), 'g+-')
-	hold on
-	index = index + 1;
-	Mu = Mu * Beta
-	
+    new_obj = @(X)(obj(X(1), X(2), Mu));
+    new_grad = @(X)(grad(X(1), X(2), Mu));
+    ta = [10:5e-1:30]; % A
+    ts = [5:5e-1:40]; % S
+    [tAA,tSS] = meshgrid(ta,ts);
+    tDrag = arrayfun(obj, tAA, tSS, ones(size(tAA))*Mu);
+    contour(tAA,tSS,tDrag,50);
+    qn_log = MajorIterationHistory();
+    xk = QuasiNewtonBFGS(linesearch, new_obj, new_grad, xk, e_g, e_a, e_r, qn_log, ls_parameters)
+    %x_star = ConjugateGradient(linesearch, new_obj, new_grad, X_0, e_g, e_a, e_r, qn_log, ls_parameters)
+    [drag, grad_drag] = plane1.m_DragForce(xk(1), xk(2))
+
+    hold on
+    plot(qn_log.x(:,1),qn_log.x(:,2), 'g+-')
+    hold on
+    index = index + 1;
+    Mu = Mu * Beta
+    
 end
 %plot(x_star(1), x_star(2), 'g+')
 
@@ -102,7 +103,7 @@ end
 %============================================================
 plane2 = mdo.ComputeAirPlane(N_ult, t_over_c, W_0, rho, mu, k, e, S_wet_ratio, Velocity);
 
-%fmincon_fig = figure;
+fmincon_fig = figure;
 lb = [1; 1];
 ub= [inf; inf];
 
@@ -113,7 +114,7 @@ options = optimoptions('fmincon', 'Algorithm', 'sqp' ,'GradObj', 'on', 'GradCons
 fmin_obj = @(X)(plane2.m_DragForce(X(1), X(2)));
 nonlincon = @(X)(plane2.m_nonLinearConstraint(X(1), X(2), V_min, C_L_max));
 [x,fval, exitflag, output, lambda] = fmincon(fmin_obj,X_0,[], [], [], [], lb, ub, nonlincon, options );
-
+plot(fmin_log.x(:,1),fmin_log.x(:,2), 'bd-')
 hold on;
 plot(x(1), x(2), 'kd');
 hold on
