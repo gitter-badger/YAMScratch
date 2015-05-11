f1a1e057ba07d1b55460615a6a4cf2b56e4d8aba
YetAnotherMinion <yetanotherminion@gmail.com>
2015-05-11 10:00:09 -0500

    CA #2 more getline fun

--- CodeAbbey/src/2.c
+++ CodeAbbey/src/2.c
@@ -3,17 +3,30 @@
 #include <errno.h> /*for errno*/
 #include <stdio.h> /*provided perror*/
 #include <stdlib.h> /*provides exit*/
+#include <string.h> /*provides strsep*/
 
 
 int main(int argc, char* argv[]) {
 	char* lineptr;
 	size_t nbytes;
 	ssize_t bytes_read;
+	int N;
 	lineptr = NULL; /*cause getline to allocate buffer for us*/
 	nbytes = 0;
 	bytes_read = getline(&lineptr, &nbytes, stdin);
+	if(bytes_read < 0) {
+		perror("Error in getline");
+		exit(EXIT_FAILURE);
+	} else if(bytes_read == 0) {
+		perror("zero bytes read");
+		exit(-1);
+	}
+
 	fprintf(stdout, "[%s]\n", lineptr );
 	fflush(stdout);
+	printf("%d\n", atoi(lineptr) );
+
+
 	return 0;
 
 }
