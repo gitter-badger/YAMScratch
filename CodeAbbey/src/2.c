45703ab240de133d5879969d2bfb2a8930a93595
YetAnotherMinion <yetanotherminion@gmail.com>
2015-05-12 08:12:10 -0500

    CA #2 switched to getline

--- CodeAbbey/src/2.c
+++ CodeAbbey/src/2.c
@@ -4,32 +4,16 @@
 #include <stdio.h> /*provided perror*/
 #include <stdlib.h> /*provides exit*/
 
-#define BUFFER_SIZE 3
-#define BUFFER_END BUFFER_SIZE-1
 
 int main(int argc, char* argv[]) {
-	int nread;
-	char* buffer;
-	buffer = (char*)malloc(BUFFER_SIZE);
-	fprintf(stdout, "N: ");
-	fflush(stdout); 
-	nread = read(STDIN_FILENO, buffer, BUFFER_END);
-	buffer[BUFFER_END] = '\0'; /*forcibly null terminate*/
-	printf("Input: %s ffoo\n", buffer);
+	char* lineptr;
+	size_t nbytes;
+	ssize_t bytes_read;
+	lineptr = NULL; /*cause getline to allocate buffer for us*/
+	nbytes = 0;
+	bytes_read = getline(&lineptr, &nbytes, stdin);
+	fprintf(stdout, "[%s]\n", lineptr );
 	fflush(stdout);
-	printf("bytes: %d\n", nread);
-	fflush(stdout);
-
-	/*force a read*/
-	do {
-		nread = read(STDIN_FILENO, buffer, BUFFER_END);
-		buffer[BUFFER_END] = '\0'; /*forcibly null terminate*/
-		printf("Foo %s\n", buffer);
-		fflush(stdout);
-		printf("bar %d\n", nread);
-		fflush(stdout);
-	} while(nread);
-
 	return 0;
 
 }
