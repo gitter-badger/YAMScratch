947e661fdb13c213346121c4184a9e3fcd65c11a
YetAnotherMinion <yetanotherminion@gmail.com>
2015-05-15 08:30:31 -0500

    CA #3 generic input parsing

---
+++ CodeAbbey/inc/parseCodeAbbeyInput.h
@@ -0,0 +1,21 @@
+#ifndef _PARSE_CODE_ABBEY_
+#define _PARSE_CODE_ABBEY_
+
+#define INPUT_ARRAY_INIT(name) 									\
+	typedef struct { 												\
+		name * items; 												\
+		size_t size; 												\
+	} ia_##name##_t; 												\
+	static inline ia_##name##_t *init_##name() { 					\
+		return (ia_##name##_t*)calloc(1, sizeof(ia_##name##_t)); 	\
+	} 																\
+	static inline void destroy_##name(ia_##name##_t *p) { 			\
+		if(p) { 													\
+			free(p); 												\
+		} 															\
+	}
+
+#define InArray(name) ia_##name##_t
+
+
+#endif
---
+++ CodeAbbey/src/3.c
@@ -0,0 +1,20 @@
+#include <unistd.h> /*for getop */
+#include <stdint.h>
+#include <errno.h> /*for errno*/
+#include <stdio.h> /*provided perror*/
+#include <stdlib.h> /*provides exit*/
+#include <string.h> /*provides strsep*/
+
+#include "parseCodeAbbeyInput.h"
+
+INPUT_ARRAY_INIT(int)
+
+#define SIX 6
+
+int main(int argc, char* argv[]) {
+
+
+	InArray(int) foo;
+
+	return 0;
+}
