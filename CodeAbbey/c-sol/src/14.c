38a8c4fc18f57d0c150a13ecb133e00514b55a8e
YetAnotherMinion <yetanotherminion@gmail.com>
2015-05-21 09:01:00 -0500

    CA 14 works

--- CodeAbbey/c-sol/src/14.c
+++ CodeAbbey/c-sol/src/14.c
@@ -4,6 +4,13 @@
 #include <stdio.h> /*provided perror*/
 #include <stdlib.h> /*provides exit*/
 #include <math.h>
+#include <limits.h>
+#include <assert.h>
+
+#include "parseCodeAbbeyInput.h"
+
+VECTOR_INIT(long)
+VECTOR_INIT(char)
 
 int main(int argc, char* argv[]) {
 	unsigned N;
@@ -18,13 +25,16 @@ int main(int argc, char* argv[]) {
 	unsigned ii;
 	char operation;
 	long A, tmp;
-	tmp = N;
 	operation = '\0';
-	/*add one to N to account for the final mod operation*/
+	Vector_t(long)* num = newVector(long);
+	Vector_t(char)* op_code = newVector(char);
+	enum operations {PLUS, MINUS, MULTIPLY, DIVIDE, MODULO};
+
 	ii = 0;
+
+
 	while(operation != '%') {
 		rc = scanf(" %c %lu", &operation, &A);
-		printf("%c : %lu \n",operation, A );
 		if(rc != 2 || !rc) {
 			errno = EINVAL;
 			fprintf(stderr, "Failed on input line: %d  %d\n", ii+2, rc );
@@ -33,24 +43,46 @@ int main(int argc, char* argv[]) {
 		}
 		switch(operation) {
 			case '+':
-				tmp += A;
+				vector_push_back(char, op_code, PLUS);
+				vector_push_back(long, num, A);
 				break;
 			case '-':
-				tmp -= A;
-				break;
+				vector_push_back(char, op_code, MINUS);
+				vector_push_back(long, num, A);
 			case '*':
-				tmp *= A;
+				vector_push_back(char, op_code, MULTIPLY);
+				vector_push_back(long, num, A);
 				break;
 			case '/':
-				tmp /= A;
+				vector_push_back(char, op_code, DIVIDE);
+				vector_push_back(long, num, A);
 				break;
 			case '%':
-				tmp %= A;
+				vector_push_back(char, op_code, MODULO);
+				vector_push_back(long, num, A);
 				break;
 		}
 		++ii; /*for error reporting only*/
-
+	}
+	/*now the modulo should be the last item on the vector*/
+	assert(num->elms == op_code->elms);
+	long mod = num->items[num->elms-1];
+	tmp = N % mod;
+	for(ii = 0; ii < num->elms-1; ++ii){
+		switch(op_code->items[ii]) {
+			case PLUS:
+				tmp += (num->items[ii] % mod); 
+				tmp %= mod;
+				break;
+			case MULTIPLY:
+				tmp *= (num->items[ii] % mod);
+				tmp %= mod;
+				break;
+		}
 	}
 	printf("%lu\n", tmp);
+
+	vector_destroy(long, num);
+	vector_destroy(char, op_code);
 	return 0;
 }
