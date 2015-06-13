80926bf4de9e08c67f43bc714409095b256a7b6f
YetAnotherMinion <yetanotherminion@gmail.com>
2015-06-12 22:59:12 -0400

    fixed one bug with swapping, looking for others

--- CodeAbbey/c-sol/inc/yam_sorting.h
+++ CodeAbbey/c-sol/inc/yam_sorting.h
@@ -8,6 +8,7 @@ extern "C" {
 #define QUICKSORT_INIT(TYPE) \
 	void yam_quicksort_##TYPE(TYPE * list, unsigned left, unsigned right, unsigned (*less_than)(TYPE*, TYPE*)) {\
 		/*find recursive case when len(list) > 3*/							\
+		printf("left = %d, right = %d\n",left, right); 	\
 		unsigned ii, jj;													\
 		TYPE* pivot;														\
 		if(left+2 < right){													\
@@ -16,29 +17,22 @@ extern "C" {
 			center = left + (right - left)/2;								\
 			TYPE tmp;														\
 			if(less_than(list+center, list+left)) {							\
-				tmp = list[center];											\
-				list[left] = list[center];									\
-				list[center] = tmp;											\
+				swap(list, center, left, tmp)								\
 			}																\
 			if(less_than(list+right, list+left)) {							\
-				tmp = list[left];											\
-				list[left] = list[right];									\
-				list[right] = tmp;											\
+				swap(list, right, left, tmp) 								\
 			}																\
 			if(less_than(list+right, list+center)) {						\
-				tmp = list[center];											\
-				list[center] = list[right];									\
-				list[right] = tmp;											\
+				swap(list, right, center, tmp) 								\
 			}																\
 			/*																\
 			* now the pivot is in the center, move							\
 			* the pivot to the right-1 position								\
 			*/																\
-			tmp = list[right - 1];											\
-			list[right-1] = list[center];									\
-			list[center] = tmp;												\
+			swap(list, center, right-1, tmp)								\
 			/*pivot is the pointer to the array elements*/					\
 			pivot = list + right - 1;										\
+			printf("pivot = %d\n", *pivot); 	\
 			/*begin partitioning array*/									\
 			ii = left;														\
 			jj = right - 1;													\
@@ -47,32 +41,30 @@ extern "C" {
 				while(--jj, less_than(pivot, list+jj));						\
 				if( ii < jj) {												\
 					/*swap the values*/										\
-					tmp = list[ii];											\
-					list[ii] = list[jj];									\
-					list[jj] = tmp;											\
+					swap(list, ii, jj, tmp)									\
 				} else {													\
 					break;													\
 				}															\
 			}																\
 			/*restore pivot*/												\
-			tmp = list[right - 1];											\
-			list[right-1] = list[ii];										\
-			list[ii] = tmp;													\
+			swap(list, ii, right - 1, tmp)									\
 			/*now recurse on smaller paritions*/							\
 			yam_quicksort_##TYPE(list, left, ii - 1, less_than); 			\
 			yam_quicksort_##TYPE(list, ii + 1, right, less_than); 			\
 		} else {															\
+			printf("calling insertion sort\n"); \
 			/* 																\
 			* perform simple insertion sort, we recycle the pivot variable 	\
 			* here it is really just a temp var to save on swaps 			\
 			*/																\
-			unsigned offset;														\
+			unsigned offset;												\
 			offset = (right - left) * sizeof(TYPE);							\
 			for(ii = 1; ii <= offset; ++ii) {								\
 				jj = ii;													\
 				pivot = list + ii;											\
 				while(jj > 0 && less_than(pivot, list+jj-1)) {				\
 					list[jj] = list[jj-1];									\
+					printf("swapped element\n");	\
 					--jj;													\
 				}															\
 				/*get the actual value of the pivot to copy in*/			\
@@ -80,6 +72,12 @@ extern "C" {
 			}																\
 		}																	\
 	}
+
+#define swap(list, a, b, tmp) 			\
+	tmp = list[a];						\
+	list[a] = list[b];					\
+	list[b] = tmp;
+
 
 #define yam_quicksort(TYPE, list, left, right, fnptr) \
 	yam_quicksort_##TYPE(list, left, right, fnptr)
