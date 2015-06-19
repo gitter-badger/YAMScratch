a193b22e09b021094ecdf8d2c50274d16929d8d7
YetAnotherMinion <yetanotherminion@gmail.com>
2015-06-19 18:44:28 -0400

    roughing out internals of interpreter

--- CodeAbbey/c-sol/inc/brainfuck_interpreter.h
+++ CodeAbbey/c-sol/inc/brainfuck_interpreter.h
@@ -5,17 +5,49 @@ extern "C" {
 #ifndef _YAM_BRAINFUCK_INTERPRETER_
 #define _YAM_BRAINFUCK_INTERPRETER_
 
-/*use a linked list for the tape*/
-typedef struct {
+#include <stdio.h>
+
+/*use the vector as the stack for the extended mode*/
+#include "parseCodeAbbeyInput.h"
+VECTOR_INIT(int)
+
+/*use a linked list for the data tape*/
+struct TapeNode {
+	struct TapeNode* next;
+	struct TapeNode* prev;
+	int cell;
+};
+
+/*
+* The intention is to provide me with a debugger as I learn brainfuck,
+* so it is very likely that I will give invalid instructions
+* like infinite loops, so we will try to expose as much information as possible
+* in the debug version as I will be the only one using it, and I need all
+* the help I can get:
+* include the index in the memory so the memory debugger can tell us where we are
+* in the brainfuck data tape
+* */
+struct TapeNodeDebug {
+	struct TapeNodeDebug* next;
+	struct TapeNodeDebug* prev;
+	int cell;
+	long index;
+};
 
 
-} TapeNode;
+signed brainfuck_evaluate_file(FILE* fp, int debug);
 
-signed brainfuck_evaluate(FILE* fp);
+signed brainfuck_evaluate_buffer(char* src, size_t nbytes, int debug);
 
-signed brainfuck_evaluate(char* src);
+/*private implementation of interpreter*/
+signed _eval_buffer(char* src, size_t nbytes, struct TapeNode* cursor, Vector_t(int)* stack);
 
-signed brainfuck_evaluate_internal(char* src, size_t nbytes, );
+
+/*
+* debug version will check for overflows and underflows data cells and emmit warning
+* to stderr
+*/
+signed _evaluate_buffer_debug(char* src, size_t nbytes, struct TapeNodeDebug* cursor, Vector_t(int)* stack);
 
 #endif
 
--- CodeAbbey/c-sol/src/brainfuck_interpreter.c
+++ CodeAbbey/c-sol/src/brainfuck_interpreter.c
@@ -1,11 +1,41 @@
 
 #include "brainfuck_interpreter.h"
+/*remember the Vector_t macro was imported in the header for this file*/
 
-signed brainfuck_evaluate(FILE* fp, int debug) {
-
+signed brainfuck_evaluate_file(FILE* fp, int debug) {
+	return 0;
 }
 
-signed brainfuck_evaluate(char* src, int debug) {
-
+signed brainfuck_evaluate_buffer(char* src, size_t nbytes, int debug) {
+	return 0;
 }
 
+signed _eval_buffer(char* src, size_t nbytes, struct TapeNode* cursor, Vector_t(int)* stack) {
+	return 0;
+}
+/*only adds to atape, will not remove any nodes*/
+signed _eval_buffer_debug(char* src, size_t nbytes, struct TapeNodeDebug* cursor, Vector_t(int)* stack ) {
+	/*keep track of where in the data buffer we are*/
+	unsigned long instruction_offset;
+	/*keep track of where in the */
+	signed long  data_offset;
+	char* instr_ptr;
+
+	instr_ptr = src;
+	/*dereference the instruction pointer each time*/
+	switch(*instr_ptr) {
+		case '+': /*increment current value of cell*/
+		case '-':
+		case '>':
+		case '[':
+		case ']':
+		case ',':
+		case '.':
+		case ';':
+		case ':':
+		case '#':
+		case '$':
+			break;
+	}
+	return 0;
+}
