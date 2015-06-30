8e038eedd81e7014c6f4f77e8eae29682f2c1042
YetAnotherMinion <yetanotherminion@gmail.com>
2015-06-30 13:56:13 -0400

    CA 108 works

---
+++ CodeAbbey/c-sol/src/108.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+	unsigned long N;
+	int rc;
+	errno = 0;
+	rc = scanf(" %lu ", &N);
+	if(rc != 1) {
+		perror("failed to read input");
+		exit(-1);
+	}
+	unsigned* result;
+	errno = 0;
+	result = (unsigned*)malloc(N * sizeof(unsigned));
+	if(result == NULL) {
+		perror("failed to allocate result buffer");
+		exit(-1);
+	}
+	/*
+	** find the number of intersectons in a star with
+	** M vertices and a step of T
+	*/
+	unsigned ii, M, T;
+	for(ii = 0; ii < N; ++ii) {
+		errno = 0;
+		rc = scanf(" %u %u ", &M, &T);
+		if(rc != 2) {
+			fprintf(stderr, "input read error at line %u\n", ii +2 );
+			fflush(stderr);
+			perror("errno: ");
+			exit(-1);
+		}
+		/*
+		** each line crosses 2(T-1) other lines, there are N lines
+		** and each intersection is made up of two lines, so we divide
+		** by two to prevent double counting of intersections
+		*/
+		result[ii] = (T-1) * M;
+	}
+
+	for(ii = 0; ii < N; ++ii) {
+		printf("%d ",result[ii]);
+	}
+	printf("\n");
+	free(result);
+	return 0;
+}
