d19ab09fcfc95d120633b4733072c02d08f4d72f
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-04 20:42:57 -0400

    reference serial version works

---
+++ CodeAbbey/c-sol/src/57.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+	unsigned long N;
+	int rc;
+	errno = 0;
+	rc = scanf(" %lu ", &N);
+	if(rc != 1) {
+		perror("failed to read input");
+		exit(-1);
+	}
+	double* result;
+	errno = 0;
+	result = (double*)malloc(N * sizeof(double));
+	if(result == NULL) {
+		perror("failed to allocate result buffer");
+		exit(-1);
+	}
+	/*
+	** find the total sum of a 2d6 roll using large random number
+	** each line of input is two random numbers for dice rools
+	*/
+	unsigned ii;
+	for(ii = 0; ii < N; ++ii) {
+		errno = 0;
+		/*read input so we can perform cal*/
+		rc = scanf(" %lf ", result+ii);
+		if(rc != 1) {
+			fprintf(stderr, "input read error at line %u\n", ii +2 );
+			fflush(stderr);
+			perror("errno: ");
+			exit(-1);
+		}
+	}
+	double prev, tmp;
+	prev = result[0];
+	for(ii = 1; ii < N-1; ++ii) {
+		tmp = prev + result[ii] + result[ii+1];
+		tmp /= 3.0;
+		/*save the current value since it will be replaced*/
+		prev = result[ii];
+		result[ii] = tmp;
+	}
+	for(ii = 0; ii < N; ++ii) {
+		printf("%15.15lf ",result[ii]);
+	}
+	printf("\n");
+	free(result);
+	return 0;
+}
