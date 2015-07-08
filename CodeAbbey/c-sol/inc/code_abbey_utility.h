8cb72e32e0f75907623cd3a6744be92479b89915
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-08 15:08:21 -0400

    testing selection sort

--- CodeAbbey/c-sol/inc/code_abbey_utility.h
+++ CodeAbbey/c-sol/inc/code_abbey_utility.h
@@ -7,6 +7,40 @@ extern "C" {
 #include <stdio.h>
 
 signed array_checksum(long const * array, size_t size, long* checksum);
+/*sort in place*/
+
+#define selection_sort_init(TYPE) \
+	extern signed selection_sort_##TYPE(TYPE * array, size_t size, int (*cmp)(TYPE *,  TYPE *)) { \
+		/*sanity checks*/	\
+		if(size < 2) {	\
+			return 0;	\
+		}	\
+		if(array == NULL) {	\
+			return -1;	\
+		}	\
+		TYPE tmp;	\
+		size_t ii, jj, max;	\
+		/*make N passes through array*/	\
+		for(ii = 1; ii < size; ++ii) {	\
+			max = 0;	\
+			for(jj = 1; jj <= size - ii; ++jj) {	\
+				/*if element less than max*/	\
+				if((*cmp)(array+jj, array+max)) {	\
+				} else {	\
+					printf("%ld %ld\n", array[jj], array[max]);	\
+					max = jj;	\
+				}	\
+			}	\
+			printf("[0 - %ld] \n", jj);	\
+			/*swap maximum element with end element*/	\
+			tmp = array[size-ii];	\
+			array[size-ii] = array[max];	\
+			array[max] = tmp;	\
+		}	\
+		return 0;	\
+	}
+
+#define selection_sort(TYPE, array, size, cmp) selection_sort_##TYPE(array, size, cmp)
 
 #endif
 
