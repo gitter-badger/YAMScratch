730f5ebca58bd1f9becace36257310c7b3721e92
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-14 21:33:56 -0400

    store more information to prevent recomputing it

--- CodeAbbey/c-sol/src/103.c
+++ CodeAbbey/c-sol/src/103.c
@@ -3,13 +3,33 @@
 #include <stdio.h>
 #include <errno.h>
 #include <assert.h>
-#include <string.h>
+
+/*becuse we are using a long as a bitmask*/
+#define MAX_BITS 64
+
+struct MaskData {
+	unsigned long key; /*key we use can use to compare objects*/
+	size_t len; /*number of bits set in mask*/
+	unsigned char* bits; /*array indicating which indices are set, is malloced and freed*/
+};
+
+void print_MaskData(struct MaskData* m) {
+	printf("Key: %lu\n", m->key);
+	/*printf("Len: %lu\n", m->len);*/
+	printf("Bits: ");
+	size_t ii;
+	for(ii = 0; ii < m->len; ++ii) {
+		printf("%u ", m->bits[ii]);
+	}
+	printf("\n");
+}
 
 int main(int argc, char const *argv[])
 {
 	int rc;
-	unsigned ii, jj, A, I, E, T;
+	unsigned ii, jj, kk, A, I, E, T;
 	unsigned long N;
+	/*read first line with unknown length of data*/
 	char * lineptr;
 	size_t nbytes;
 	ssize_t bytes_read;
@@ -43,7 +63,7 @@ int main(int argc, char const *argv[])
 		I |= A<<N;
 		++N;
 	}
-	if(N > 64) {
+	if(N > MAX_BITS) {
 		/*we cannot represent more than 64 bits using long
 		* so we must give an error in this implementation*/
 		fprintf(stderr, "More than 64 states given by input\n");
@@ -51,17 +71,21 @@ int main(int argc, char const *argv[])
 		exit(-1);
 	}
 	fclose(stream);
+	/* Allocate a buffer to record which indices are set */
+	unsigned* index_buffer;
+	index_buffer = (unsigned*)malloc(N* sizeof(unsigned));
+	if(index_buffer == NULL) {
+		perror("failed to allocate a buffer");
+		exit(-1);
+	}
 	/*
-	* Allocate a buffer to hold the N bitmasks, this
-	* is purposefully zeroed in case there is no rule
-	* for an index, so it will have no effect on latter 
-	* computations. This is done because input rule order
-	* is not defined.
+	* Store each masks, we will only read up to N masks from the file,
+	* this is defined input format 
 	*/
-	unsigned long* masks;
+	struct MaskData* _masks;
 	errno = 0;
-	masks = (unsigned long*)calloc(N, sizeof(unsigned long));
-	if(masks == NULL) {
+	_masks = (struct MaskData*)malloc(N * sizeof(struct MaskData));
+	if(_masks == NULL) {
 		perror("failed to allocate masks buffer");
 		exit(-1);
 	}
@@ -72,14 +96,16 @@ int main(int argc, char const *argv[])
 		perror("failed to allocate metadata buffer");
 		exit(-1);
 	}
-	/*read in the N mappings*/
+	/*ptr inside temporary buffer that creates */
+	unsigned* cursor;
+	/*read in the N masks*/
 	for(ii = 0; ii < N; ++ii) {
+		printf("iteration %u", ii);
 		errno = 0;
 		bytes_read = getline(&lineptr, &nbytes, stdin);
 		if(bytes_read < 0) {
 			/*this checks for when there is only EOF */
-			perror("Must input a number");
-			exit(-1);
+			break;
 		} else if(bytes_read == 1) {
 			/*check for empty line which should not happen*/
 			if(*lineptr == '\n') {
@@ -97,26 +123,47 @@ int main(int argc, char const *argv[])
 		* the colon character delimeter follows*/
 		rc = fscanf(stream, "%u %c", &E, &colon);
 		assert(colon == ':');
-		for(jj = 0;;) {
+		/*walk over buffer starting from begining*/
+		cursor = index_buffer;
+		/*the below code depends on these two fields beign zeroed*/
+		_masks[E].len = 0;
+		_masks[E].key = 0;
+		for(jj = 0; jj < MAX_BITS; ++jj) {
 			rc = fscanf(stream," %u ", &A);
 			if(rc == EOF) {
 				break;
 			}
 			/*make sure shift will fit in mask, and prevents walking off
 			* edge of array*/
-			assert(A < 64);
-			masks[E] |= 1<<A;
+			assert(A < MAX_BITS);
+			_masks[E].key |= 1<<A;
+			_masks[E].len++;
+			/*store and advance buffer*/
+			*cursor++ = A;
 			if(metadata[2*A+1]++ == 0) {
 				metadata[2*A] = E;
 			}
-			++jj;
 		}
 		fclose(stream);
+		/*allocate an appropriate size buffer for the MaskData
+		* instance and copy valid contents of buff into*/
+		_masks[E].bits = (unsigned char*)malloc(_masks[E].len* sizeof(unsigned char));
+		for(kk = 0; kk < _masks[E].len; ++kk) {
+			_masks[E].bits[kk] = index_buffer[kk];
+		}
 	}
 	for(ii = 0; ii < N; ++ii) {
 		printf("Bit %u is flipped %u times\n", ii, metadata[ii*2+1]);
 	}
 	free(lineptr);
 	printf("\n");
+
+	/*free each of the MaskData bits arrays*/
+	for(ii = 0; ii < N; ++ii) {
+		print_MaskData(_masks+ii);
+		printf("\n");
+		free(_masks[ii].bits);
+	}
+	free(_masks);
 	return 0;
 }
