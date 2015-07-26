7c0c29d1846b53b85c09917ab1059e9cb094a99f
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-26 19:36:26 -0400

    random number generation working locally

---
+++ CodeAbbey/py-sol/dynamic page/main.py
@@ -0,0 +1,35 @@
+import flask
+import urllib2
+import re
+import time
+#from google.appengine.ext import db
+from google.appengine.ext import ndb
+
+import random
+
+app = flask.Flask(__name__)
+app.config['DEBUG'] = False
+
+class Employee(ndb.Model):
+	name = ndb.StringProperty(indexed = True)
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/random-page/<int:value>')
+def build_random_page(value=0):
+	out_val = value * random.randint(10, 1000)
+	return flask.render_template('secret_number.html', secret_value = out_val)
+	#return "The secret value is ",200
+
+@app.route('/query-string')
+def parse_query():
+	#flask.request
+	#return flask.render_template()
+	return "Hello there"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Sorry, nothing at this URL.', 404
